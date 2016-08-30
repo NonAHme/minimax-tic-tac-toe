58262653249453d6326fae8e8dae5adb9c1762da
isaiah <isaiah@entegrity.co>
2016-08-30 17:10:18 +0100

    python 3 compatible and code clean up

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import random
 from collections import namedtuple
@@ -27,7 +28,8 @@ def winner(pos):
     return any(win.issubset(pos) for win in WINNING_LINES)
 
 
-def gen_next_states(board):
+def moves(board):
+    "Board -> [Board] all valid moves"
     os, xs, _ = board
     return (new_move(board, i) for i in '123456789' if i not in (os | xs))
 
@@ -37,7 +39,7 @@ def is_terminal(board):
     return (set('123456789') == (os | xs)) or winner(os) or winner(xs)
 
 
-def score(board, depth=0):
+def score(board, alpha=-math.inf, beta=math.inf):
     os, xs, current = board
     if winner(os):
         return -1
@@ -46,25 +48,25 @@ def score(board, depth=0):
     if is_terminal(board):
         return 0
     best = min if current == 'x' else max
-    return best(score(b) for b in gen_next_states(board))
-
+    return best(score(b) for b in moves(board)) 
 
 def next_move(board):
     _, _, current = board
     best = min if current == 'x' else max
-    moves = gen_next_states(board)
-    return best(moves, key=score)
+    next_states = moves(board)
+    return best(next_states, key=score)
 
 
 def print_b(board, file=sys.stdout):
     os, xs, who = board
-
+    
     def val_at(i):
         if i in os: return ' O'
         if i in xs: return ' X'
         return '  '
-    turn = 'x' if who == 'o' else 'o'
-    print("it is {turn}'s turn".format(turn=turn.upper()), file=file)
+    turn = 'X'.upper() if who == 'o' else 'O'
+    description = "game over !"  if is_terminal(board) else "it is {turn}'s turn".format(turn=turn)
+    print(description, file=file)
     print('', file=file)
     for row, cols in enumerate(['123', '456', '789']):
         if row in (1, 2):
@@ -73,28 +75,28 @@ def print_b(board, file=sys.stdout):
         print(line, end=' \n', file=file)
 
 
-def game(start=True):
-    if not start:
-        pos = yield mk_board()
-        current_state = mk_board(xs=str(pos))
-    else:
-        current_state = mk_board(xs=random.choice('123456789'))
-
+def game(start=True, state=None):
+    current_state = state or mk_board(who='o')
     while not is_terminal(current_state):
-        state = next_move(current_state)
-        next_pos = yield state
-        current_state = new_move(state, next_pos)
+        ai_move = next_move(current_state)
+        next_pos = yield ai_move
+        current_state = new_move(ai_move, next_pos)
+        yield current_state
 
-
-def play_game():
-    y_n = raw_input('X starts first do you want to start ? [y,n]')
+def play_game(file=sys.stdout):
+    y_n = input('X starts first do you want to start ? [y,n]')
     if y_n in ['y', 'Y']:
-        play = game(state, start=False)
+        print_b(mk_board(who='o'), file=file)
+        pos = input('select position for your move \n')
+        plays = game(start=False, state=mk_board(xs=str(pos)))
     else:
-        play = game()
-    b = next(play)
-    print_b(b)
-    while True:
-        pos = raw_input('select position for your move')
-        board = play.send(pos)
-        print_b(board)
+        plays = game()
+    for board in plays:
+        print_b(board, file=file)
+        if is_terminal(board):
+            break
+        pos = input('select position for your move \n')
+        board = plays.send(pos)
+        print_b(board, file=file)
+    
+    print("game over ", file=file)
